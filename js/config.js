453a8fe1ef4684984b15abef6bf9770d9183b8a1
nasik-dh <nasikdhiu@gmail.com>
2025-10-21 13:13:18 +0530

    Delete js directory

--- js/config.js
+++
@@ -1,5 +0,0 @@
-// Replace with your Google Apps Script Web App URL
-const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbwUVfITgBkLSDiiugsWsxta1O5mfSBPGK9aYzHtYWCIEa-rkAYPehThDkAe2GCDxbHm/exec';
-
-// You'll get this URL after deploying your Google Apps Script
-// It will look like: https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec
